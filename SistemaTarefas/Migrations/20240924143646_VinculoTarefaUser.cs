72c81a7c5eb5a0d16629484d540509e339e07e60
Paulo Souza <paulosouzx14@gmail.com>
2024-09-24 16:11:58 +0100

    Update de todas minhas migrations

---
+++ SistemaTarefas/Migrations/20240924143646_VinculoTarefaUser.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaTarefas.Migrations
+{
+    public partial class VinculoTarefaUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserID",
+                table: "Tasks",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_UserID",
+                table: "Tasks",
+                column: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_UserID",
+                table: "Tasks",
+                column: "UserID",
+                principalTable: "Users",
+                principalColumn: "ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_UserID",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_UserID",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "Tasks");
+        }
+    }
+}
